b3d3c8c4c201eef8c430d3151f9fbe7698e55686
KossssTek <91365451+KossssTek@users.noreply.github.com>
2021-10-11 22:20:58 +0500

    DZ #1

---
+++ #1.py
@@ -0,0 +1,7 @@
+duration = int(input('Ведите время в секундах'))
+days = duration //(60 * 60 * 24)
+hours = (duration - days * (60 * 60 * 24)) //(60 * 60)
+minutes = (duration - days * (60 * 60 * 24) - hours * (60 * 60)) // 60
+seconds = duration - days * (60 * 60 * 24) - hours * (60 * 60) - minutes *60
+print(days, 'дн', hours, 'час', minutes, 'мин', seconds, 'сек' )
+
